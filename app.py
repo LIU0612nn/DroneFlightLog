67b84404d9fc610922b88e44c5e39f3cf978f4fe
LIU0612nn <3577463836@qq.com>
2026-09-12 21:40:18 +0800

    Add: production config, Procfile, and gunicorn requirements

--- app.py
+++ app.py
@@ -253,4 +253,7 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # 优先使用云平台分配的端口（环境变量 PORT），如果没有，才用本地的 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', debug=True, port=port)
+    
